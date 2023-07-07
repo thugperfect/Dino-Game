ddfd5dd3e1a654f4276b37b77ae9dd70aafc96d1
thugperfect <meenamani9444@gmail.com>
2023-07-07 09:49:04 +0000

    added gameOver

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const you  = document.getElementById("you");
+const obj = document.getElementById("obj")
 
 function jump(){
     if(you.classList != "jump"){
@@ -7,6 +8,13 @@ function jump(){
             you.classList.remove("jump")
         },500)
     }
+    let run = setInterval(()=>{
+        let youTop = parseInt(window.getComputedStyle(you).getPropertyValue("top"))
+        let objEnd = parseInt(window.getComputedStyle(obj).getPropertyValue("left"))
+        if(objEnd < 30 && objEnd >0 && youTop>430){
+            window.alert("game over!")
+        }
+    },10)
   
 }
 document.addEventListener("keypress",(event)=>{
