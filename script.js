495764fbd0eabf514e1c686ef4a2c09b1200f063
thugperfect <meenamani9444@gmail.com>
2023-07-07 14:53:43 +0000

    better start

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 const you  = document.getElementById("you");
-const obj = document.getElementById("obj")
+const obj = document.getElementById("obj");
+const b = document.getElementById("body")
 
 function jump(){
+    
     if(you.classList != "jump"){
         you.classList.add("jump")
         setTimeout(()=>{
             you.classList.remove("jump")
-        },500)
+        },600)
     }
-    if(obj.classList != "run"){
+   
+    obj.style.display = "block"
+    if(run = true){
         obj.classList.add("run")
       
     }
-    let run = setInterval(()=>{
-        let youTop = parseInt(window.getComputedStyle(you).getPropertyValue("top"))
-        let objEnd = parseInt(window.getComputedStyle(obj).getPropertyValue("left"))
-        if(objEnd < 30 && objEnd >0 && youTop>430){
-         alert("game over")  
-        }
-    },10)
+    else{
+        obj.classList.remove("run")
+    }
+   
   
 }
 
-document.addEventListener("keypress",(event)=>{
-    jump()
-})
+    document.addEventListener("keypress",(event)=>{
+        jump()
+        run = setInterval(()=>{
+            let youTop = parseInt(window.getComputedStyle(you).getPropertyValue("top"))
+            let objEnd = parseInt(window.getComputedStyle(obj).getPropertyValue("left"))
+            if(objEnd < 40 && objEnd >0 && youTop>430){
+        
+                alert("Game Over")
+    
+            }
+            
+        },10)
+    })
+    
 
+  
 function game(f){
     let btn = document.getElementById("start")
     let body = document.getElementById("mbody")
